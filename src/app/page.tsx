e968a0f1bbb448bb1bac03109070d3ea12754ef1
Subodh S <s.subodh98@gmail.com>
2024-01-21 09:21:06 +0530

    push to git

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+'use client';
+import GameBoard from './components/GameBoard';
+
+export default function Home() {
+    return (
+        <div className="h-screen flex justify-center items-center">
+            <GameBoard />
+        </div>
+    );
+}
